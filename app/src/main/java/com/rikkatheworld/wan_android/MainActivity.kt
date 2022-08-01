ddd5aefe3d6a01fbd649484c2e07a68dbfb17526
rikkaxie <rikkaxie@futunn.com>
2022-08-01 16:37:57 +0800

    first commit

---
+++ app/src/main/java/com/rikkatheworld/wan_android/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.rikkatheworld.wan_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rikkatheworld.wan_android.ui.theme.Wan_androidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Wan_androidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Wan_androidTheme {
+        Greeting("Android")
+    }
+}
